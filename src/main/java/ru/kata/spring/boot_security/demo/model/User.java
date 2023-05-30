464bb7d6b071e2d0222f537cae8609dfc9d6bde7
Frendzyfy <125387546+Frendzyfy@users.noreply.github.com>
2023-05-31 02:54:22 +0500

    second

--- src/main/java/ru/kata/spring/boot_security/demo/model/User.java
+++ src/main/java/ru/kata/spring/boot_security/demo/model/User.java
@@ -47,7 +47,7 @@ public class User implements UserDetails {
     @Column(name = "salary")
     private int salary;
 
-    @ManyToMany(fetch = FetchType.EAGER)
+    @ManyToMany(fetch = FetchType.LAZY)
     @JoinTable(name = "users_roles",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "roles_id"))
